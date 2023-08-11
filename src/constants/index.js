9455bf7bac9a88519ba2072d68017a7ff12fa041
nikhil1610 <nikhilgautam106@gmail.com>
2023-08-11 21:38:49 +0530

    community_sidebar done

---
+++ src/constants/index.js
@@ -0,0 +1,52 @@
+import {Dsa_wizards, Meme,Tech_birds} from '../assets'
+
+const communities = [
+    {
+        name:'DSA Community',
+        members:'161',
+        logo: Dsa_wizards,
+        posts:[
+            {
+
+            }
+        ]
+    },
+    {
+        name:'DSA Wizards',
+        members: '11',
+        logo: Dsa_wizards,
+        posts:[
+            {
+
+            }
+        ]
+    },
+    {
+        name:'MEME',
+        members:'9',
+        logo: Meme,
+        posts:[
+            {
+
+            }
+        ]
+    },
+    {
+        name:'Tech Birds',
+        members: '1',
+        logo: Tech_birds,
+        posts:[
+            {
+
+            }
+        ]
+    }
+];
+
+const follow_people = [
+    {
+
+    }
+];
+
+export { communities, follow_people }
